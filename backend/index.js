16e1f9ea0d846f4d227d95b4c3bd6ae7fe07253c
Andy Kirby <andykirby.uk@gmail.com>
2022-09-26 16:36:24 +0100

    Production

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ const app = express()
 app.enable('trust proxy')
 
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join('frontend/build')));
 app.get("/*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve("frontend", "build", "index.html"));
     });
 
 //listen for requests
