410a5f0278c45038e9e7f1d7e5cd80ccd0eccd56
Andy Kirby <andykirby.uk@gmail.com>
2022-09-26 09:50:08 +0100

    Production

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,11 @@ const cors = require('cors')
 const app = express()
 app.enable('trust proxy')
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("/*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-      });
-}
+app.use(express.static(path.join(__dirname, './frontend/build')));
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+    });
+
 
 //listen for requests
 app.listen(process.env.PORT, () => {
@@ -22,7 +21,7 @@ app.listen(process.env.PORT, () => {
 })
 
 //middleware
-// app.use(express.static(path.join(__dirname, './frontend/build')));
+
 // app.use(express.json());
 // app.use(cors());
 
