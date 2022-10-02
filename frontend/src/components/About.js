a2cc32540e6504baea89f38a554b11be56449c40
Andy Kirby <andykirby.uk@gmail.com>
2022-10-02 17:14:48 +0100

    Production

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,9 +5,9 @@ import { motion } from "framer-motion"
 
 const About = () => {
     return (
-        <motion.div className="about-container" id="top" initial={{opacity: 0}} animate={{opacity: 1, transition:{duration: 2}}} exit={{opacity: 0}}>
+        <motion.div className="about-container" id="top" exit={{opacity: 0}}>
             <div className="about-left">
-                <img className="andykirby" src={require('../assets/images/andykirby.png')} alt={""}></img>
+                <motion.img className="andykirby" src={require('../assets/images/andykirby.png')} alt={""} initial={{opacity: 0}} animate={{opacity: 1, transition:{duration: 0.8}}}></motion.img>
                 <div style={{ background: "#dfdfdf", height: "3px", width: "60px", margin: "25px", borderRadius: "6px" }}></div>
                 <p className="about-text"><span style={{ color: "#9e9e9e" }}>"</span>Hi! I'm Andy... I am a software & application developer from Leeds. With a drive for <span style={{ color: "#000000"}}>innovation</span> and a <span style={{ color: "#000000"}}>serious passion</span> for code I specialise in
                 cross-platform desktop software & web applications as well as iOS & Android mobile application development using the React-Native framework. Always <span style={{ color: "#000000"}}>connected with technology</span> my mission is to create a digital platform delivering tailored & optimised solutions to all challenges.<br></br>
